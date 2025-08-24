f50a1fafd4da3a41f0ddbc2ae42d541ce260f5cf
venicios <venicios@debian.venuz>
2025-08-23 23:15:27 -0300

    ajuste na geraçãod e tabela no prisma

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/commo
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
-import { PrismaClient, Book } from '@prisma/client';
+import { Book } from '@prisma/client';
 
 
 @Controller('books')
