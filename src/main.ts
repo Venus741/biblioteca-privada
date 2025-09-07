681f625b2022accc98138f08e31e68e1df29dd14
venicios <venicios@debian.venuz>
2025-09-07 20:45:42 -0300

    Habilitando cors para consumo de API no front

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { METHODS } from 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: "*",
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE"
+  })
+
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
